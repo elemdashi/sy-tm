4e5de680832b9978f860c8d09e4f107ee86f4bdc
wanggaohan <1005778698@qq.com>
2019-10-15 15:10:55 +0800

    axios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,10 +7,41 @@
 
 <script>
 // @ is an alias to /src
+import Vue from 'vue'
+import axios  from 'axios'
 
+Vue.use( axios)
 
 export default {
   name: 'home',
- 
+created(){
+  
+    this.getData()
+},
+
+ methods:{
+    getData() {
+            console.log("获取后台数据");
+           
+            axios.get('/data/shouye.json')
+                .then(function (response) {
+                
+                    console.log("111");
+                    console.log(response)
+
+                    // that.dyList = response.data.data.dy;
+                    // that.techList = response.data.data.tech;
+                    // that.autoList = response.data.data.auto;
+                    // that.moneyList = response.data.data.money;
+                    // that.sportList = response.data.data.sports;
+                    // that.warList = response.data.data.war;
+                    // that.entList = response.data.data.ent;
+                    // that.touTiaoList = response.data.data.toutiao;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+ }
 }
 </script>
